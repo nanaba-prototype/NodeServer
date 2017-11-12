ff4a5f27b44bc7b8a8216c50927f5adf4b20a182
stories2 <stories2@naver.com>
2017-11-12 22:42:12 +0900

    index/jsonTest module : sample of request response data
    recognize request method, content type and set response status ,data

--- functions/index.js
+++ functions/index.js
@@ -7,4 +7,41 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     console.log("Print hello message");
     response.send("Hello from Firebase!");
 });
- 
+
+exports.jsonTest = functions.https.onRequest((request, response) => {
+
+    var resultMethod = ""
+    var resultContentType = ""
+    switch(request.method) {
+        case 'POST':
+            console.log("post request")
+    break;
+            case 'GET':
+                console.log("get request")
+    break;
+                default:
+                    break;
+}
+    resultMethod = request.method
+    switch(request.get('content-type')) {
+        case 'application/json':
+            console.log("hello json")
+    break;
+            default:
+                console.log("who are u?")
+    break;
+}
+    resultContentType = request.get('content-type')
+    requestMessage = request.body
+    console.log(requestMessage);
+    // requestMessage = JSON.parse(requestMessage)
+    requestBodyData = []
+    for (key in requestMessage) {
+        console.log("key: " + key + " value: " + requestMessage[key])
+        requestBodyData.push([key, requestMessage[key]])
+    }
+    responseMessage = {"result" : "ok", "method" : resultMethod, "content-type" : resultContentType, "request message": requestBodyData}
+
+   response.setHeader('Content-Type', 'application/json');
+   response.status(200).send(JSON.stringify(responseMessage))
+});
