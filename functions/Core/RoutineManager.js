c265c52166f54a8cb6fea9ba17c6be9238835730
Kim HyunWoo <stories2@naver.com>
2018-03-11 00:12:11 +0900

    RoutineManaber module : save template of routine

--- functions/Core/RoutineManager.js
+++ functions/Core/RoutineManager.js
@@ -1,14 +1,34 @@
 exports.AddRoutine = function (admin, response, responseManager, generateManager, bodyData) {
 
-    tempResponse = {'rid': global.defineManager.NOT_AVAILABLE}
-
-    responseManager.TemplateOfResponse(tempResponse, global.defineManager.HTTP_REQUEST_ERROR, response)
-
     dateStr = new Date().toISOString()
     rid = generateManager.CreateHash(bodyData["uid"] + dateStr)
 
     global.logManager.PrintLogMessage("RoutineManager", "AddRoutine", "add new routine data to database rid: " + rid,
         global.defineManager.LOG_LEVEL_INFO)
+
+    routineDataTemplate = {
+        "areYouUseThis" : "Yes",
+        "commentLength" : 0,
+        "commentUser" : {
+        },
+        "description" : "",
+        "favorite" : 0,
+        "favoriteUser" : [],
+        "good" : 0,
+        "routineLength" : 0,
+        "season" : [],
+        "steps" : [],
+        "time" : [],
+        "title" : "",
+        "uid" : "",
+        "uploadDate" : "",
+        "writer" : ""
+    }
+
+    status = admin.database().ref("/Routine/" + rid + "/").set(routineDataTemplate);
+
+    tempResponse = {'rid': rid}
+    responseManager.TemplateOfResponse(tempResponse, global.defineManager.HTTP_SUCCESS, response)
 }
 
 exports.DelRoutine = function () {
