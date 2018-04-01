c170dea595cd87967b48d408f3807e00a014c300
Kim HyunWoo <stories2@naver.com>
2018-04-01 23:24:04 +0900

    ResponseManager module : accept cross domain

--- functions/Utils/ResponseManager.js
+++ functions/Utils/ResponseManager.js
@@ -4,5 +4,7 @@ exports.TemplateOfResponse = function (responseJsonDicData, responseCode, respon
     template["data"] = responseJsonDicData
 
     response.setHeader('Content-Type', 'application/json');
+    response.setHeader("Access-Control-Allow-Origin", "*")
+    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     response.status(responseCode).send(JSON.stringify(template));
 }
