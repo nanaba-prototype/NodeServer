2ef4388cea89f0e0512d1845516db5b1986d5c7f
Kim HyunWoo <stories2@naver.com>
2018-04-03 23:42:05 +0900

    Hosting : search routine <test page added (not working)>

---
+++ public/js/view/SearchRoutineManager.js
@@ -0,0 +1,7 @@
+function SetServerRequestResult(resultData) {
+    $("#serverResult").text(resultData)
+}
+function SetSignInInfo(displayName, uid) {
+    $("#displayName").text("Display name: " + displayName)
+    $("#uid").text("UID: " + uid)
+}
