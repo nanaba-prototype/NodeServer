22afe994e96f16b8ff08aef87f82656f663b5946
Kim HyunWoo <stories2@naver.com>
2018-04-08 14:30:50 +0900

    Hosting : DefineManager <fix domain link>

--- public/js/settings/DefineManager.js
+++ public/js/settings/DefineManager.js
@@ -4,7 +4,7 @@ LOG_LEVEL_DEBUG = 2
 LOG_LEVEL_WARN = 3
 LOG_LEVEL_ERROR = 4
 
-DOMAIN = "http://localhost:5000/nanaba-server/us-central1/"
+DOMAIN = "https://us-central1-nanaba-server.cloudfunctions.net/"
 // http://localhost:5000/nanaba-server/us-central1/
 // https://us-central1-nanaba-server.cloudfunctions.net/
 SUB_DIRECTORY = "app/"
