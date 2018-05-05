18689d31c542b3ef32a13f7de96d67fbfcb18089
Kim HyunWoo <stories2@naver.com>
2018-05-05 16:17:30 +0900

    Hosting : <Create new routine> if press upload btn, then converting process will begin

--- public/js/view/CreateNewRoutineManager.js
+++ public/js/view/CreateNewRoutineManager.js
@@ -11,26 +11,30 @@ $().ready(function () {
         PrintLogMessage("CreateNewRoutineManager", "ready", "clicked increase routine", LOG_LEVEL_DEBUG)
         IncreaseRoutineNum()
     })
+    $("#uploadMyRoutine").click(function () {
+        PrintLogMessage("CreateNewRoutineManager", "ready", "clicked upload routine", LOG_LEVEL_DEBUG)
+        GetStepInfo()
+    })
 })
 
 function DecreaseRoutineNum() {
     if(routineStepNum > 1) {
         routineStepNum -= 1
+
+        PopRoutineStep()
     }
     PrintLogMessage("CreateNewRoutineManager", "DecreaseRoutineNum", "now routine num: " + routineStepNum, LOG_LEVEL_DEBUG)
     $("#routineNum").text(routineStepNum)
-
-    PopRoutineStep()
 }
 
 function IncreaseRoutineNum() {
     if(routineStepNum < 13) {
         routineStepNum += 1
+
+        PushRoutineStep()
     }
     PrintLogMessage("CreateNewRoutineManager", "IncreaseRoutineNum", "now routine num: " + routineStepNum, LOG_LEVEL_DEBUG)
     $("#routineNum").text(routineStepNum)
-
-    PushRoutineStep()
 }
 
 function PushRoutineStep() {
@@ -55,6 +59,37 @@ function ResetRoutineStep() {
     while(PopRoutineStep()){
 
     }
+}
+
+function GetStepInfo() {
+    listOfStepsData = {}
+    stepLength = routineStepStack.length
+    checkingCounter = 0
+    PrintLogMessage("CreateNewRoutineManager", "GetStepInfo", "getting step data len: " + stepLength, LOG_LEVEL_DEBUG)
+    for(checkingCounter = 0; checkingCounter < stepLength; checkingCounter += 1) {
+        indexOfStep = routineStepStack[checkingCounter]
+        indexOfStepData = {}
+
+        indexOfStepData["name"] = $(indexOfStep).find("#stepName").val()
+        indexOfStepData["productBrand"] = $(indexOfStep).find("#productBrand").val()
+        indexOfStepData["productName"] = $(indexOfStep).find("#productName").val()
+
+        frequency = []
+        frequency.push($(indexOfStep).find("#frequencyDaily").val())
+        frequency.push($(indexOfStep).find("#frequencyWeekly").val())
+        frequency.push($(indexOfStep).find("#frequencyMonthly").val())
+        frequency.push($(indexOfStep).find("#frequencyAsNeeded").val())
+        indexOfStepData["frequency"] = frequency
+
+        indexOfStepData["rating"] = $(indexOfStep).find("#rating").val()
+        indexOfStepData["tags"] = [$(indexOfStep).find("#tags").val()]
+        indexOfStepData["advice"] = $(indexOfStep).find("#advice").val()
+
+        PrintLogMessage("CreateNewRoutineManager", "GetStepInfo", "#" + checkingCounter + " data: " + JSON.stringify(indexOfStepData), LOG_LEVEL_DEBUG)
+
+        listOfStepsData[checkingCounter] = indexOfStepData
+    }
+    return listOfStepsData
 }
 
 PushRoutineStep()
