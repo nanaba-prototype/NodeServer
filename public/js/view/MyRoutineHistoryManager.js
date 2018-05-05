dbd201a7f21caf70eae707dacf7724b7b4f5cfdb
Kim HyunWoo <stories2@naver.com>
2018-05-06 00:12:32 +0900

    Hosting : <My routine history> control view now working

---
+++ public/js/view/MyRoutineHistoryManager.js
@@ -0,0 +1,36 @@
+function SetServerRequestResult(resultData) {
+    $("#serverResult").text(resultData)
+}
+
+function PushRoutineInfoRow(infoText, detailBtnUrl) {
+    routineInfoStackPoint = routineHistoryStack.length
+
+    routineInfoObj = $("#templateOfRoutineRow").clone()
+    routineInfoObj.removeAttr("hidden")
+    routineInfoObj.appendTo("#bodyOfTable")
+    routineInfoObj.attr("id", "routineInfo" + routineInfoStackPoint)
+
+    infoTextObj = routineInfoObj.find("#routineInfo")
+    infoTextObj.text(infoText)
+
+    detailBtnObj = routineInfoObj.find("#detailBtn")
+    detailBtnObj.attr("data-url", detailBtnUrl)
+
+    routineHistoryStack.push(routineInfoObj)
+}
+
+function PopRoutineInfoRow() {
+    if(routineHistoryStack.length <= 0) {
+        return false
+    }
+    stackPoint = routineHistoryStack.length - 1
+    $(routineHistoryStack[stackPoint]).remove()
+    routineHistoryStack.pop()
+    return true
+}
+
+function ResetRoutineInfoRow() {
+    while(PopRoutineInfoRow()) {
+
+    }
+}
