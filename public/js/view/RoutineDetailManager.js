c9616279f60c1ed61c6c6d947386edecb6d3e967
Kim HyunWoo <stories2@naver.com>
2018-05-13 02:52:49 +0900

    Hosting : <RoutineDetailManager> add api templates

---
+++ public/js/view/RoutineDetailManager.js
@@ -0,0 +1,27 @@
+function SetServerRequestResult(resultData) {
+    $("#serverResult").text(resultData)
+}
+
+function PushStepData() {
+
+}
+
+function PopStepData() {
+
+}
+
+function ResetSteps() {
+
+}
+
+function SearchRoutineDetailData() {
+
+}
+
+function SetRoutineBasicInfo() {
+
+}
+
+function SetRoutineTopCommentInfo() {
+
+}
