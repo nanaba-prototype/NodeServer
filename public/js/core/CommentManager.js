cdd65f99fe7c7b48ece60485bcacc222381d78f7
Kim HyunWoo <stories2@naver.com>
2018-06-02 13:10:33 +0900

    Hosting <comment history> : add template of script

--- public/js/core/CommentManager.js
+++ public/js/core/CommentManager.js
@@ -19,11 +19,25 @@ CommentManager.prototype.AddNewComment = function(rid, commentMsg){
 }
 
 CommentManager.prototype.AddNewCommentSuccess = function (data) {
-    PrintLogMessage("CommentManager", "AddNewComment", "add new comment successfully", LOG_LEVEL_DEBUG)
+    PrintLogMessage("CommentManager", "AddNewComment", "add new comment successfully", LOG_LEVEL_INFO)
     SetServerRequestResult(JSON.stringify(data))
 }
 
 CommentManager.prototype.AddNewCommentFail = function (errorText, errorStatus) {
-    PrintLogMessage("CommentManager", "AddNewComment", "something wrong with add new comment: " + errorText, LOG_LEVEL_DEBUG)
+    PrintLogMessage("CommentManager", "AddNewComment", "something wrong with add new comment: " + errorText, LOG_LEVEL_WARN)
+    SetServerRequestResult(SERVER_RESULT_FAILED)
+}
+
+CommentManager.prototype.SearchCommentList = function (rid) {
+    PrintLogMessage("CommentManager", "SearchCommentList", "search comment list rid: " + rid, LOG_LEVEL_INFO)
+}
+
+CommentManager.prototype.SearchCommentListSuccess = function (data) {
+    PrintLogMessage("CommentManager", "SearchCommentList", "search comment successfully", LOG_LEVEL_INFO)
+    SetServerRequestResult(JSON.stringify(data))
+}
+
+CommentManager.prototype.SearchCommentListFail = function (errorText, errorStatus) {
+    PrintLogMessage("CommentManager", "SearchCommentList", "something wrong with searching comment list: " + errorText, LOG_LEVEL_INFO)
     SetServerRequestResult(SERVER_RESULT_FAILED)
 }
---
+++ public/js/view/CommentHistoryManager.js
@@ -0,0 +1,19 @@
+function SetServerRequestResult(resultData) {
+    $("#serverResult").text(resultData)
+}
+
+function PushCommentItem() {
+    
+}
+
+function PopCommentItem() {
+    
+}
+
+function ResetCommentTable() {
+    
+}
+
+function SearchTargetRoutineComment() {
+    
+}
