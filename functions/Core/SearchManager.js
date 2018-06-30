ad5833a2c26d30c49b906b4721d5787f49ef560e
Kim HyunWoo <stories2@naver.com>
2018-06-30 17:16:38 +0900

    SearchManager : sex, skin type, skin concern, age search option now working

--- functions/Core/SearchManager.js
+++ functions/Core/SearchManager.js
@@ -15,6 +15,9 @@ exports.SearchRoutines = function (admin, request, response, responseManager) {
 
         routineDataSnapshot = JSON.parse(JSON.stringify(snapshot))
         for(indexOfRoutineKey in routineDataSnapshot) {
+
+            passThisRoutine = false
+
             global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "searching key: " + indexOfRoutineKey, global.defineManager.LOG_LEVEL_DEBUG);
 
             indexOfRoutineData = routineDataSnapshot[indexOfRoutineKey]
@@ -23,6 +26,7 @@ exports.SearchRoutines = function (admin, request, response, responseManager) {
             usernameOption = databaseSearchQuery["username"]
             if(usernameOption != null) {
                 if(indexOfRoutineData["writer"].includes(usernameOption)) {
+                    passThisRoutine = true
                     global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok username passed: " + indexOfRoutineKey, global.defineManager.LOG_LEVEL_DEBUG)
                 }
                 else {
@@ -36,26 +40,141 @@ exports.SearchRoutines = function (admin, request, response, responseManager) {
                 global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "username option is null", global.defineManager.LOG_LEVEL_WARN)
             }
 
-            // sexOption = databaseSearchQuery[""]
+            // time option
             timeOption = databaseSearchQuery["time"]
             if(timeOption != null) {
                 indexOfRoutineTimeStr = timeOption.join()
                 for(indexOfTime in timeOption) {
                     if(indexOfRoutineTimeStr.includes(timeOption[indexOfTime])) {
+                        passThisRoutine = true
                         global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok time passed: " + indexOfRoutineKey, global.defineManager.LOG_LEVEL_DEBUG)
                         break;
                     }
                 }
             //    cannot found
-                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "no matching result <time>", global.defineManager.LOG_LEVEL_WARN);
+            //     global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "no matching result <time>", global.defineManager.LOG_LEVEL_WARN);
             }
             else{
             //    time option is null
                 global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "time option is null", global.defineManager.LOG_LEVEL_WARN)
             }
-            tempResult = {}
-            tempResult[indexOfRoutineKey] = indexOfRoutineData
-            searchQueryResult.push(tempResult)
+
+            // sex option
+            sexOption = databaseSearchQuery["sex"]
+            if(sexOption != null) {
+                if(!indexOfRoutineData.hasOwnProperty("sex")) {
+                    global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "this routine has not sex", global.defineManager.LOG_LEVEL_WARN)
+                    passThisRoutine = false
+                    continue;
+                }
+                for(indexOfSex in sexOption) {
+                    if(indexOfRoutineData["sex"] == sexOption[indexOfSex]) {
+                        passThisRoutine = true
+                        global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok sex passed", global.defineManager.LOG_LEVEL_INFO)
+                        break;
+                    }
+                }
+            }
+            else {
+                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "sex option is null", global.defineManager.LOG_LEVEL_WARN)
+            }
+
+            // skin type option
+            skinTypeOption = databaseSearchQuery["skinType"]
+            if(skinTypeOption != null) {
+                if(!indexOfRoutineData.hasOwnProperty("skinType")) {
+                    global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "this routine has not skin type", global.defineManager.LOG_LEVEL_WARN)
+                    passThisRoutine = false
+                    continue;
+                }
+                for(key in indexOfRoutineData["skinType"]) {
+                    if(skinTypeOption.includes(indexOfRoutineData["skinType"][key])) {
+                        passThisRoutine = true
+                        global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok skin type passed", global.defineManager.LOG_LEVEL_INFO)
+                        break;
+                    }
+                }
+            }
+            else {
+                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "skinType option is null", global.defineManager.LOG_LEVEL_WARN)
+            }
+
+            // skin concern
+            skinConcernOption = databaseSearchQuery["skinConcern"]
+            if(skinConcernOption != null) {
+                if(!indexOfRoutineData.hasOwnProperty("skinConcern")) {
+                    global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "this routine has not skin concern", global.defineManager.LOG_LEVEL_WARN)
+                    passThisRoutine = false
+                    continue;
+                }
+                for(key in indexOfRoutineData["skinConcern"]) {
+                    if(skinTypeOption.includes(indexOfRoutineData["skinConcern"][key])) {
+                        passThisRoutine = true
+                        global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok skin concern passed", global.defineManager.LOG_LEVEL_INFO)
+                        break;
+                    }
+                }
+            }
+            else {
+                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "skin concern option is null", global.defineManager.LOG_LEVEL_WARN)
+            }
+
+            // age
+            ageOption = databaseSearchQuery["ageGroup"]
+            if(ageOption != null) {
+                if(!indexOfRoutineData.hasOwnProperty("birthYear")) {
+                    global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "this routine has not age", global.defineManager.LOG_LEVEL_WARN)
+                    passThisRoutine = false
+                    continue;
+                }
+                date = new Date()
+                currentYear = date.getFullYear()
+                writerAge = Number(currentYear) - Number(indexOfRoutineData["birthYear"])
+                for(indexOfSearchAge in ageOption) {
+                    switch (ageOption[indexOfSearchAge]) {
+                        case global.defineManager.SEARCH_AGE_GROUP_18_29:
+                            if(18 <= writerAge && writerAge < 29) {
+                                passThisRoutine = true;
+                                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok age passed", global.defineManager.LOG_LEVEL_INFO)
+                                break;
+                            }
+                            break;
+                        case global.defineManager.SEARCH_AGE_GROUP_30_39:
+                            if(30 <= writerAge && writerAge < 39) {
+                                passThisRoutine = true;
+                                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok age passed", global.defineManager.LOG_LEVEL_INFO)
+                                break;
+                            }
+                            break;
+                        case global.defineManager.SEARCH_AGE_GROUP_40_49:
+                            if(40 <= writerAge && writerAge < 49) {
+                                passThisRoutine = true;
+                                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok age passed", global.defineManager.LOG_LEVEL_INFO)
+                                break;
+                            }
+                            break;
+                        case global.defineManager.SEARCH_AGE_GROUP_50:
+                            if(50 <= writerAge) {
+                                passThisRoutine = true;
+                                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "ok age passed", global.defineManager.LOG_LEVEL_INFO)
+                                break;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            else {
+                global.logManager.PrintLogMessage("SearchManager", "SearchRoutines", "age option is null", global.defineManager.LOG_LEVEL_WARN)
+            }
+
+            //passed result
+            if(passThisRoutine) {
+                tempResult = {}
+                tempResult[indexOfRoutineKey] = indexOfRoutineData
+                searchQueryResult.push(tempResult)
+            }
         }
 
         searchQueryResult = searchQueryResult.slice(-global.defineManager.SEARCH_RETURN_LIMIT)
